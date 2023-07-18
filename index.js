f7affd53c81408394d102a879ec717605a106c48
Sahil <garg07825@gmail.com>
2023-07-18 11:49:25 +0530

    remove file and add login ejs from starting

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import path from "path";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+const app = express();
+// connected the nodejs with mongodb
 mongoose
   .connect("mongodb://127.0.0.1:27017/backend", {
     useNewUrlParser: true,
@@ -14,12 +16,12 @@ mongoose
     console.log(err);
   });
 
-const mongooseSchema = new mongoose.Schema({
+// making a schema
+const userSchema = new mongoose.Schema({
   name: String,
   email: String,
 });
-const Message = mongoose.model("Message", mongooseSchema);
-const app = express();
+const User = mongoose.model("User", userSchema);
 
 // using static folder
 app.use(express.static(path.join(path.resolve(), "public")));
@@ -28,44 +30,30 @@ app.use(cookieParser());
 // Setting up the view engine
 app.set("view engine", "ejs");
 
-// making a function 
+// making a function
 const isAuthenticated = (req, res, next) => {
   const { token } = req.cookies;
-  if (token) {
-    next();
-  } else {
-    res.render("login");
-  }
-}
+  if (token) next(); 
+  else res.render("login");
+};
 
-app.get("/",isAuthenticated, (req, res, next) => {
-  res.render("logout")
+app.get("/", isAuthenticated, (req, res) => {
+  res.render("logout");
 });
 
-app.get("/add", async (req, res) => {
-  await Message.create({ name: "ai", email: "haga@hhh" });
-  res.send("Success");
-  console.log("working");
-});
+// login handler && adding a data in database
+app.post("/login", async(req, res) => {
+  const { name, email } = req.body;
+ const user = await User.create({name,email});
 
-app.get("/success", (req, res) => {
-  res.send("success");
-});
-
-app.post("/", (req, res) => {
-  Message.create({ name: req.body.name, email: req.body.email }).then(() => {
-    res.redirect("/");
-    console.log("working");
-  });
-});
-
-app.post("/login", (req, res) => {
-  res.cookie("token", "maiaagyo", {
+  res.cookie("token", user._id, {
     httpOnly: true,
     expires: new Date(Date.now() + 60 * 1000),
   });
   res.redirect("/");
 });
+
+// logout handler
 app.get("/logout", (req, res) => {
   res.cookie("token", null, {
     httpOnly: true,
@@ -74,6 +62,7 @@ app.get("/logout", (req, res) => {
   res.redirect("/");
 });
 
+// server connection
 app.listen(3000, (req, res) => {
   console.log("app is working");
 });
